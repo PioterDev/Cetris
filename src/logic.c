32a8d02e0dfbe3b37c26f623b1a27148db389116
PioterDev <71720941+PioterDev@users.noreply.github.com>
2024-05-03 13:45:27 +0200

    Another failed attempt at multithreading events

--- src/logic.c
+++ src/logic.c
@@ -10,6 +10,8 @@
 
 DWORD WINAPI logicLoop(void* params) {
     logicLoopParameters* parameters = params;
+    ProgramParameters* programParameters = parameters->programParameters;
+    Queue* q = parameters->eventQueue;
 
     while(true) {
         if(*(parameters->logicStatus) == STOP)break;
@@ -17,9 +19,33 @@ DWORD WINAPI logicLoop(void* params) {
         WaitForSingleObject(parameters->logicMutex, INFINITE);
         WaitForSingleObject(parameters->tilesMutex, INFINITE);
         
-        void* action; //change later accordingly
-        while(dequeueAlt(parameters->actionQueue, action)) {
+        while(q->size == 0)Sleep(1);
 
+        SDL_Event event; //change later accordingly
+        while(dequeue(q, &event)) {
+            if(programParameters->debugLog != NULL)fprintf(programParameters->debugLog, "Dequeued event\n");
+            if(event.type == SDL_QUIT) {
+                *(parameters->logicStatus) = STOP;
+                break;
+            }
+            else if(event.type == SDL_KEYDOWN) {
+                SDL_Keycode key = event.key.keysym.sym;
+                Keymap keymap = parameters->programParameters->keymap;
+                SDL_Rect* rect = &parameters->tiles[1]->rect;
+
+                if(key == keymap.movePieceLeft) {
+                    if(rect->x >= 10)rect->x -= 32;
+                }
+                else if(key == keymap.movePieceRight) {
+                    if(rect->x <= programParameters->screen_width)rect->x += 32;
+                }
+                else if(key == keymap.dropSoft) {
+                    if(rect->y <= programParameters->screen_height)rect->y += 32;
+                }
+                else if(key == keymap.rotateClockwise) {
+                    if(rect->y >= 32)rect->y -= 32;
+                }
+            }
         }
 
         ReleaseMutex(parameters->tilesMutex);
